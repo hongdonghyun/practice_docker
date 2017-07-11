99ac49c6117065c11577480ba20b71ede480d562
hongdonghyun <hong4367@gmail.com>
2017-07-11 22:03:31 +0900

    first commit

---
+++ django_app/config/settings/debug.py
@@ -0,0 +1,13 @@
+from .base import *
+
+config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
+
+WSGI_APPLICATION = 'config.wsgi.debug.application'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+DEBUG = True
+ALLOWED_HOSTS = config_secret_debug["django"]["allowed_hosts"]
+
+print('@@@@@@ DEBUG:', DEBUG)
+print('@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
---
+++ django_app/config/settings/deploy.py
@@ -0,0 +1,13 @@
+from .base import *
+
+config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
+
+WSGI_APPLICATION = 'config.wsgi.deploy.application'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+DEBUG = False
+ALLOWED_HOSTS = config_secret_deploy["django"]["allowed_hosts"]
+
+print('@@@@@@ DEBUG:', DEBUG)
+print('@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
